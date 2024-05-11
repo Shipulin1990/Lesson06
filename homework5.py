25abff9ab8f4d445fc6776782ea6f9d6c08f3390
Shipulin1990 <168572651+Shipulin1990@users.noreply.github.com>
2024-05-11 20:20:10 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+list = ['kiwi', 'mango', 'lemon', 'cherry', 'plum']
+print(list)
+print(list[0])
+print(list[4])
+print(list[2:5])
+list[2] = 'pineapple'
+print(list)
+dictionary = {'kiwi': 'киви', 'mango': 'манго', 'lemon': 'лемон', 'cherry': 'вишня', 'plum': 'слива'}
+print(dictionary)
+print(dictionary.values())
+dictionary.update({'pineapple': 'ананас'})
+print(dictionary)
